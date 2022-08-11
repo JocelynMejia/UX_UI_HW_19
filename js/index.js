7a0c4acb6e640b8062c0add46a5def9bfbe1853d
JocelynMejia <54966059+JocelynMejia@users.noreply.github.com>
2022-08-11 18:11:49 -0400

    Updated

---
+++ js/index.js
@@ -0,0 +1,12 @@
+//$(document).ready(function(){
+
+    $('#mywork').on('click',function() {
+        alert('you have clicked');
+        $(this).css('text-color', 'red');
+    });
+
+//});
+
+
+
+console.log("your index.js file is loaded correctlty!")
